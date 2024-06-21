4fe0f8abc0054c21e96f97b66cf113803b18ef20
JFernandez0524 <njphonefix@gmail.com>
2024-06-21 09:34:06 -0400

    styled components

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  HomeLayout,
+  About,
+  Landing,
+  Error,
+  Newsletter,
+  Cocktail,
+} from './pages';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'cocktail',
+        element: <Cocktail />,
+      },
+      {
+        path: 'newsletter',
+        element: <Newsletter />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    <App />
+  </React.StrictMode>
+);
---
+++ src/pages/Newsletter.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Newsletter = () => {
+  return <div>Newsletter Page</div>;
+};
+
+export default Newsletter;
